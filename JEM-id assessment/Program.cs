c78eabe6c5425f57d302b493ee96cc19ad3e35f0
Jim Dijkxhoorn <jimdijkxhoorn@gmail.com>
2023-04-07 15:48:59 +0200

    Removed file

--- JEM-id assessment/Program.cs
+++ JEM-id assessment/Program.cs
@@ -41,7 +41,7 @@ void SeedData(IHost app)
 }
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (!app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
